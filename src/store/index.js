adce703a53abf98807f161e07c0b242eabbf3a25
Yurii Berch <you@example.com>
2019-07-26 18:38:46 +0300

    add test plugin to vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -6,9 +6,18 @@ import counter from './modules/counter';
 
 Vue.use(Vuex);
 
+const testPlugin = store => {
+  store.subscribe((mutation, state) => {
+    console.log(mutation)
+    // called after every mutation.
+    // The mutation comes in the format of `{ type, payload }`.
+  })
+}
+
 export default new Vuex.Store({
   modules: {
     user,
     counter
-  }
+  },
+  plugins: [testPlugin]
 });
